358f4dcea0d6023e5879303c635bf6c5d022687a
Vladimir Semyonov <tempname11@yandex-team.ru>
2018-04-24 16:16:36 +0300

    fix: warn only when there is no mapping

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function remap(mapping, key) {
   if (!mapping) return key;
   var result = mapping[key];
-  if (process.env.NODE_ENV === 'development') {
+  if (!result && process.env.NODE_ENV === 'development') {
     console.error('Key "' + key + '" not found in className mapping:', mapping);
   }
   return result;
